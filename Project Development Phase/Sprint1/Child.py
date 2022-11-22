bec4442934408c9fdf8963aa4b4d30d4c35b87e8
Pooja-Selvarajan <88940276+Pooja-Selvarajan@users.noreply.github.com>
2022-11-22 14:06:30 +0530

    Update Child.py

--- Project Development Phase/Sprint1/Child.py
+++ Project Development Phase/Sprint1/Child.py
@@ -3,35 +3,33 @@ import collections
 import wiotp.sdk.device
 import time
 myConfig = {
-    "identity" : {
-        "orgId" :"mcf86g",
-        "typeId" : "Child_Location",
-        "deviceId" : "childlocation"
+    "identity" :{
+        "orgId" :"ylse17",
+        "typeId" : "Android",
+        "deviceId" : "ylse17"
     },
     "auth":{
-        "token":"12345678"
+        "token":"WORLDSpretty@1811"
         }
 }
 
 client = wiotp.sdk.device.DeviceClient(config=myConfig ,logHandlers=None)
 client.connect()
 while True:
-    name = "Trichy"
-    #child is in school(SARANATHAN)
-    latitude = 10.7560
-    longitude = 78.6513
+    name = "salem"
+    #child is in safe(Salem)
+    #latitude = 11.664325
+    #ongitude = 78.146011
     
-    #child is in playground(SWAMIMALAI)
-    #latitude = 10.9569
-    #longitude = 79.3260
-    
-    #out of the network(JAMMU & KASHMIR)
-    #latitude = 32.7266
-    #longitude = 74.8570
+    #child is in playing area(Coimbatore)
+    latitude = 11.004556
+    longitude = 76.961632
 
-    #child is in home(TANJORE)
-    #latitude = 10.7828
-    #longitude = 79.1318
+    #child in school(Chennai)
+    #latitude = 13.067439
+    #ongitude = 80.237617
+
+    
     myData = {'latitude' :latitude, 'longitude':longitude}
     client.publishEvent(eventId="status",msgFormat="json",data=myData,qos=0,onPublish=None)
     print("Data published to IBM IoT platform : ",myData)
